436e81b631a6f7484f9729e4e38d9f67cbddeeff
Luke Kosner <29708877+lukekosner@users.noreply.github.com>
2023-04-22 22:13:34 -0400

    v1

--- app/page.tsx
+++ app/page.tsx
@@ -1,124 +1,123 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import { FaGithub, FaTwitter, FaArrowRight, FaEnvelope } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="min-h-screen bg-gray-200 text-black dark:bg-gray-900 dark:text-white p-5">
+      <div
+        id="header"
+        className="h-12 flex flex-row items-center justify-between"
+      >
+        <h1 className="text-2xl">Luke Kosner</h1>
+        <div className="flex flex-row space-x-3">
+          <Link href="https://twitter.com/Luke_Kosner">
+            <FaTwitter className="text-2xl" />
+          </Link>
+          <Link href="https://github.com/LukeKosner">
+            <FaGithub className="text-2xl" />
+          </Link>
         </div>
       </div>
+      <div id="columns" className="flex flex-row min-h-screen">
+        <div id="line" className="w-1 mt-3 bg-black dark:bg-white rounded-xl" />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <div id="text" className="flex flex-col p-5 space-y-12">
+          <div className="space-y-3">
+            <h1 className="text-5xl font-bold">
+              Crafting Digital Experiences to Tell Stories.
+            </h1>
+            <p className="font-lg">
+              NYC-based student, developer, and pixel pusher. Using TypeScript,
+              Python, and Swift for a better world.
+            </p>
+          </div>
+
+          <div id="updates" className="space-y-3">
+            <div className="flex flex-row">
+              <h1 className="text-3xl font-bold">Updates</h1>
+              <span className="relative flex h-2 w-2">
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-500 opacity-75"></span>
+                <span className="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
+              </span>
+            </div>
+
+            <div className="p-3 rounded-xl border border-green-500">
+              <p>
+                I am bringing Gamle back into development. I am currently doing
+                bug fixes and hope to release it soon.
+              </p>
+              <p className="font-mono">- 4/22/23</p>
+            </div>
+          </div>
+
+          <div id="projects" className="space-y-3">
+            <h1 className="text-3xl font-bold">Projects</h1>
+            <div className="space-y-1">
+              <Link
+                href="https://tap.lukekosner.com"
+                className="flex flex-row space-x-3 items-center hover:underline"
+              >
+                <h2 className="text-2xl">The Auschwitz Project</h2>
+                <FaArrowRight />
+              </Link>
+              <p>
+                Sharing the primary accounts of Auschwitz through artificial
+                intelligence.
+              </p>
+            </div>
+            <div className="space-y-1">
+              <Link
+                href="/gamle"
+                className="flex flex-row space-x-3 items-center pointer-events-none"
+              >
+                <h2 className="text-2xl  hover:underline">Gamle</h2>
+                <p className="text-black bg-yellow-400 rounded-full px-2 py-1">
+                  <p>Soon</p>
+                </p>
+              </Link>
+              <p>
+                A simple and joyful way to play Wordle-style games on your
+                iPhone.
+              </p>
+            </div>
+            <div className="space-y-1">
+              <Link
+                href="/ayudamigo"
+                className="flex flex-row space-x-3 items-center pointer-events-none"
+              >
+                <h2 className="text-2xl  hover:underline">Ayudamigo</h2>
+                <p className="bg-red-400 rounded-full px-2 py-1">
+                  <p>Hopefully 🤞</p>
+                </p>
+              </Link>
+              <p>
+                A better purpose-based translator focusing on formal letters and
+                messages.
+              </p>
+            </div>
+          </div>
+
+          <div id="contact" className="space-y-3">
+            <h1 className="text-xl font-bold">Contact</h1>
+            <div className="flex flex-row space-x-3">
+              <Link href="mailto:hey@lukekosner.com">
+                <FaEnvelope className="text-2xl" />
+              </Link>
+              <Link href="https://twitter.com/Luke_Kosner">
+                <FaTwitter className="text-2xl" />
+              </Link>
+              <Link href="https://github.com/LukeKosner">
+                <FaGithub className="text-2xl" />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div id="footer" className="mt-3 flex flex-row items-center">
+        <p className="text-lg">
+          &copy; 2023 - Luke Kosner. Made in New York City, USA.
+        </p>
       </div>
     </main>
-  )
+  );
 }
