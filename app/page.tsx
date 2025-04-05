b1d9a74be72ddd005fdbfb901edf5d891f9fd9bb
Luke Kosner <luke.kosner@icloud.com>
2025-04-05 15:22:03 -0400

    v3

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link";
-import { FaGithub, FaTwitter, FaArrowRight, FaEnvelope } from "react-icons/fa";
+import {
+  FaGithub,
+  FaTwitter,
+  FaArrowRight,
+  FaEnvelope,
+  FaLinkedin,
+} from "react-icons/fa";
 
 export default function Home() {
+  const date = new Date();
+
   return (
-    <main className="min-h-screen bg-gray-200 text-black dark:bg-gray-900 dark:text-white p-5">
+    <main className="min-h-screen flex flex-col bg-gray-200 text-black dark:bg-gray-900 dark:text-white p-5">
       <div
         id="header"
         className="h-12 flex flex-row items-center justify-between"
@@ -16,9 +24,12 @@ export default function Home() {
           <Link href="https://github.com/LukeKosner">
             <FaGithub className="text-2xl" />
           </Link>
+          <Link href="https://linkedin.com/in/luke-kosner">
+            <FaLinkedin className="text-2xl" />
+          </Link>
         </div>
       </div>
-      <div id="columns" className="flex flex-row min-h-screen">
+      <div id="columns" className="flex flex-row flex-grow">
         <div id="line" className="w-1 mt-3 bg-black dark:bg-white rounded-xl" />
 
         <div id="text" className="flex flex-col p-5 space-y-12">
@@ -27,8 +38,8 @@ export default function Home() {
               Crafting Digital Experiences to Tell Stories.
             </h1>
             <p className="font-lg">
-              NYC-based student, developer, and pixel pusher. Using TypeScript,
-              Python, and Swift for a better world.
+              NYC-based student, developer, and (wannabe) pixel pusher. Using
+              TypeScript, Python, and Swift.
             </p>
           </div>
 
@@ -43,10 +54,10 @@ export default function Home() {
 
             <div className="p-3 rounded-xl border border-green-500">
               <p>
-                I am bringing Gamle back into development. I am currently doing
-                bug fixes and hope to release it soon.
+                Retooling Zekher with LangGraph to improve accuracy and decrease
+                hallucination.
               </p>
-              <p className="font-mono">- 4/22/23</p>
+              <p className="font-mono">- 4/5/25</p>
             </div>
           </div>
 
@@ -57,42 +68,12 @@ export default function Home() {
                 href="https://tap.lukekosner.com"
                 className="flex flex-row space-x-3 items-center hover:underline"
               >
-                <h2 className="text-2xl">The Auschwitz Project</h2>
+                <h2 className="text-2xl">Zekher</h2>
                 <FaArrowRight />
               </Link>
               <p>
-                Sharing the primary accounts of Auschwitz through artificial
-                intelligence.
-              </p>
-            </div>
-            <div className="space-y-1">
-              <Link
-                href="/gamle"
-                className="flex flex-row space-x-3 items-center pointer-events-none"
-              >
-                <h2 className="text-2xl  hover:underline">Gamle</h2>
-                <p className="text-black bg-yellow-400 rounded-full px-2 py-1">
-                  <p>Soon</p>
-                </p>
-              </Link>
-              <p>
-                A simple and joyful way to play Wordle-style games on your
-                iPhone.
-              </p>
-            </div>
-            <div className="space-y-1">
-              <Link
-                href="/ayudamigo"
-                className="flex flex-row space-x-3 items-center pointer-events-none"
-              >
-                <h2 className="text-2xl  hover:underline">Ayudamigo</h2>
-                <p className="bg-red-400 rounded-full px-2 py-1">
-                  <p>Hopefully 🤞</p>
-                </p>
-              </Link>
-              <p>
-                A better purpose-based translator focusing on formal letters and
-                messages.
+                Ask questions about the Holocaust and receive answers from
+                firsthand testimonies collected by Dr. David P. Boder.
               </p>
             </div>
           </div>
@@ -109,13 +90,17 @@ export default function Home() {
               <Link href="https://github.com/LukeKosner">
                 <FaGithub className="text-2xl" />
               </Link>
+              <Link href="https://linkedin.com/in/luke-kosner">
+                <FaLinkedin className="text-2xl" />
+              </Link>
             </div>
           </div>
         </div>
       </div>
       <div id="footer" className="mt-3 flex flex-row items-center">
         <p className="text-lg">
-          &copy; 2023 - Luke Kosner. Made in New York City, USA.
+          &copy; {date.getFullYear()} Luke Kosner. Made with ♥ in New York City,
+          USA.
         </p>
       </div>
     </main>
